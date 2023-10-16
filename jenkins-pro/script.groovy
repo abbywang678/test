816ff76be2b7ad0c28db887258e8afb496c26ffa
abbywang678 <abby.wang678@gmail.com>
2023-10-16 22:36:45 +0800

    specify docker path

--- jenkins-pro/script.groovy
+++ jenkins-pro/script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t abbywang678/my-app:2.0 .'
+        sh 'docker build -t abbywang678/my-app:2.0 home/abby/my-app/docker-pro/'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push abbywang678/my-app:2.0'
     }
